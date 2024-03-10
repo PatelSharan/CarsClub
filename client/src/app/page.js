3d96eb9386c75c4d767a89c4c89bc83246bcb15b
Sharan Patel <sharanpatel4@gmail.com>
2024-03-10 12:16:12 +0530

    MAIN - Made Navbar Responsive

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -11,8 +11,19 @@ export default function Home() {
   return (
     <>
       {/* HeroSection */}
-      <div className="bg-black h-[80vh] flex relative">
-        <div className="absolute right-10 w-1/3 h-full"
+      <div className="bg-black sm:h-[80vh] flex h-[40vh]">
+        <div className='flex items-center px-5 sm:px-20 flex-col justify-center w-full '>
+          <div className='w-full text-left'>
+            <h1 className="text-white sm:text-xl md:text-3xl lg:text-5xl font-semibold text-xl">WELCOME TO <span className='text-cyan-400'>CARSCLUB</span></h1>
+          </div>
+          <div className='text-left w-full'>
+            <h1 className="text-gray-300 mt-2 lg:text-lg font-semibold text-xs">DISCOVER THE THRILL OF DRIVING.</h1>
+          </div>
+          <div className='text-left w-full'>
+            <Link href={'/screener'}> <button className='MainBtnWhite w-40 mt-2'>Discover Car</button></Link>
+          </div>
+        </div>
+        <div className="relative right-5 sm:right-20 w-1/2 h-full"
           style={{
             backgroundImage: `url(${heroSectionImage.src})`,
             backgroundSize: 'contain',
@@ -20,29 +31,18 @@ export default function Home() {
             backgroundRepeat: "no-repeat"
           }}
         ></div>
-        <div className='flex items-center px-20 flex-col justify-center '>
-          <div>
-            <h1 className="text-white text-5xl font-semibold">WELCOME TO <span className='text-cyan-400'>CARSCLUB</span></h1>
-          </div>
-          <div className='text-left w-full'>
-            <h1 className="text-gray-300 mt-2 text-lg font-semibold pl-1">DISCOVER THE THRILL OF DRIVING.</h1>
-          </div>
-          <div className='text-left w-full'>
-            <Link href={'/screener'}> <button className='MainBtnWhite w-40 mt-2'>Discover Car</button></Link>
-          </div>
-        </div>
       </div>
 
 
       {/* What We Offer Section */}
-      <div className='h-80 my-3 flex border-b p-3'>
+      <div className='min-h-80 my-3 sm:flex border-b p-3'>
         <div className='h-full flex flex-col mr-4 w-full'>
           <h1 className='text-3xl font-semibold my-3'>What We Offers</h1>
-          <p className='text-sm mb-1'>Welcome to our website, where we provide a seamless experience for car enthusiasts and buyers alike. Our platform offers a sophisticated car screener, allowing users to effortlessly filter through an extensive database of vehicles based on their preferences, including make, model, price range, and more. <br />
+          <p className='text-xs mb-5 sm:text-sm'>Welcome to our website, where we provide a seamless experience for car enthusiasts and buyers alike. Our platform offers a sophisticated car screener, allowing users to effortlessly filter through an extensive database of vehicles based on their preferences, including make, model, price range, and more. <br />
             Additionally, our compare page enables users to juxtapose multiple car models side by side, facilitating informed decision-making. For added convenience and personalized experience, we offer a secure login/signup page, where users can create accounts to save their searches, compare lists, and receive tailored recommendations. <br />
             Whether you're a seasoned car aficionado or a first-time buyer, our website is your ultimate destination for all things automotive. Start your journey with us today and discover the perfect vehicle that fits your needs and desires.</p>
         </div >
-        <div className='h-full bg-black w-full overflow-hidden'>
+        <div className='min-h-full w-full overflow-hidden'>
           <Image src={porsche_911_Front} alt='Image' width={100} height={100} layout="responsive" className=" max-w-[100%] max-h-[100%] min-w-[100%] min-h-[100%]"
           ></Image>
         </div>
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,47 +1,76 @@
 'use client'
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import Link from 'next/link'
+
 
 const Navbar = () => {
 
+    const [showNavbar, setShowNavbar] = useState(false)
 
     return (
-        <>
-            <nav className='flex border-b h-14 items-center justify-between px-7 top-0 bg-white w-full z-50 sticky mulishFont'>
+        <nav className='bg-white border-b h-14 w-full z-10 top-0 flex items-center sticky'>
+            <ul className='flex'>
 
-                {/* Logo Part */}
-                <div>
-                    <Link href={'/'} className='hover:text-black text-2xl font-semibold decoration-black</li>'>CarsClub</Link>
+                {/* To open Navbar */}
+                <div className='ml-3 cursor-pointer' onClick={() => { setShowNavbar(true) }}>
+                    {/* Menu Svg */}
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
+                        <title>Open Navbar</title>
+                        <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
+                    </svg>
                 </div>
 
-                {/* Main Navbar */}
-                <ul className='flex text-gray-500 text-sm items-center '>
-                    <li className='mr-4'>
-                        <Link href={'/'} className='hover:text-black'>Home</Link>
-                    </li>
-                    <li className='mr-4'>
-                        <Link href={'/screener'} className='hover:text-black'>Screener</Link>
-                    </li>
-                    <li className='mr-4'>
-                        <Link href={'/compare'} className='hover:text-black'>Compare</Link>
-                    </li>
-
-                    {/* Buttons Part */}
-                    <div className='flex'>
-                        <li className='mr-3'>
-                            <Link href={'/login'}>
-                                <button className='MainBtnBlack'>Login</button>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href={'/signup'}>
-                                <button className='MainBtnBlack'>Sign Up</button>
-                            </Link>
-                        </li>
+                {/*<---- Responsive Navbar */}
+                {showNavbar && <div className='fixed w-[100vw] h-[100vh] top-0 left-0 z-10' onClick={() => setShowNavbar(false)}>
+                    <div className='fixed top-0 left-0 z-10'>
+                        <div className='w-64 h-[100vh] bg-black shadow-md flex flex-col items-center p-2'>
+                            <div className='mt-7 w-[100%] space-y-2 text-gray-400'>
+                                <li className='text-center'>
+                                    <Link href={'/'}>
+                                        <button className='text-gray-300 text-2xl mb-4 hover:text-white'>CarsClub</button>
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link href={'/'}>
+                                        <button className={`w-[100%] py-1 mb-2 px-3 text-left flex hover:text-white`}
+                                        >
+                                            <span className='ml-4'>Home</span>
+                                        </button>
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link href={'/screener'}>
+                                        <button className={`w-[100%] py-1 mb-2 px-3 text-left flex hover:text-white`}
+                                        >
+                                            <span className='ml-4'>Screener</span>
+                                        </button>
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link href={'/compare'}>
+                                        <button className={`w-[100%] py-1 mb-2 px-3 text-left flex hover:text-white`}
+                                        >
+                                            <span className='ml-4'>Compare</span>
+                                        </button>
+                                    </Link>
+                                </li>
+                            </div>
+                        </div>
                     </div>
-                </ul>
-            </nav >
-        </>
+                </div>}
+
+                {/* If User not Login */}
+                <div className='absolute right-3 space-x-3'>
+                    <Link href={'/login'}>
+                        <button className='MainBtnBlack'>Login</button>
+                    </Link>
+                    <Link href={'/signup'} >
+                        <button className='MainBtnBlack'>SignUp</button>
+                    </Link>
+                </div>
+
+            </ul >
+        </nav >
     )
 }
 
